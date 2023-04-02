c720ef8256c061beee01582ab51563aa7c33f591
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-02 20:52:30 +0300

    updated the file controller

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -3,16 +3,17 @@ const File = require("../models/fileModel.js");
 exports.addFile = async (req, res) => {
   try {
     const base64File = req.body.file;
+    const file_name = req.body.file_name;
 
     const fileBuffer = Buffer.from(base64File, "base64");
 
-    const fileName =
+    const uniqueFileName =
       Date.now().toString() + "-" + Math.round(Math.random() * 1e9).toString();
 
-    const filePath = path.join(__dirname, "..", "uploads", fileName);
+    const filePath = path.join(__dirname, "..", "uploads", uniqueFileName);
     await fs.promises.writeFile(filePath, fileBuffer);
 
-    const file = new File({ name: fileName, path: filePath });
+    const file = new File({ name: file_name, path: filePath });
     await file.save();
 
     res.status(200).json({ message: "File uploaded successfully" });
