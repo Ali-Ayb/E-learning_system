80d3efd0a88395cdeace5d0c4bcaa9296edd223b
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-02 16:43:11 +0300

    updated auth controller and student model

--- controllers/auth.contollers.js
+++ controllers/auth.contollers.js
@@ -1,24 +1,23 @@
-const User = require("../models/userModel")
+const Student = require("../models/studentModel");
 const jwt = require("jsonwebtoken");
-
 
 exports.register = async (req, res) => {
   const { email, password, role } = req.body;
 
   const existingUser = await User.findOne({ email });
 
-  if (existingUser) return res.status(409).json({ message: "Email already exists" })
+  if (existingUser)
+    return res.status(409).json({ message: "Email already exists" });
 
   const user = new User();
   user.email = email;
   user.password = password;
 
-
   if (role) user.role = role;
   await user.save();
-  const { password: hashedPassword, ...newUser } = user.toJSON()
+  const { password: hashedPassword, ...newUser } = user.toJSON();
   res.status(201).json(newUser);
-}
+};
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -27,11 +26,13 @@ exports.login = async (req, res) => {
   if (!user) return res.status(404).json({ message: "Invalid Credentials" });
 
   const isMatched = user.matchPassword(password);
-  if (!isMatched) return res.status(404).json({ message: "Invalid Credentials" });
+  if (!isMatched)
+    return res.status(404).json({ message: "Invalid Credentials" });
 
-  const token = jwt.sign({ id: user._id, email: user.email }, process.env.SECRET_KEY);
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.SECRET_KEY
+  );
 
-  res.json({ token })
-
-}
-
+  res.json({ token });
+};
--- models/studentModel.js
+++ models/studentModel.js
@@ -24,14 +24,14 @@ const studentSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function (next) {
+studentSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.matchPassword = async function (password) {
+studentSchema.methods.matchPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
