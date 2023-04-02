28921b6468b1bada52899a4f217dbdae8fa99037
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-02 21:13:24 +0300

    added the necessary requires in file controller

--- controllers/file.controllers.js
+++ controllers/file.controllers.js
@@ -1,25 +1,22 @@
+const fs = require("fs");
+const path = require("path");
 const File = require("../models/fileModel.js");
 
 exports.addFile = async (req, res) => {
-  try {
-    const base64File = req.body.file;
-    const file_name = req.body.file_name;
-    const file_ext = req.body.ext;
+  const base64File = req.body.file;
+  const file_name = req.body.file_name;
+  const file_ext = req.body.file_ext;
 
-    const fileBuffer = Buffer.from(base64File, "base64");
+  const fileBuffer = Buffer.from(base64File, "base64");
 
-    const uniqueFileName =
-      Date.now().toString() + "-" + Math.round(Math.random() * 1e9).toString();
+  const uniqueFileName =
+    Date.now().toString() + "-" + Math.round(Math.random() * 1e9).toString();
 
-    const filePath = path.join(__dirname, "..", "uploads", uniqueFileName);
-    await fs.promises.writeFile(filePath, fileBuffer);
+  const filePath = path.join(__dirname, "..", "uploads", uniqueFileName);
+  await fs.promises.writeFile(filePath, fileBuffer);
 
-    const file = new File({ name: file_name + "." + file_ext, path: filePath });
-    await file.save();
+  const file = new File({ name: file_name + "." + file_ext, path: filePath });
+  await file.save();
 
-    res.status(200).json({ message: "File uploaded successfully" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
+  res.status(200).json({ message: "File uploaded successfully" });
 };
