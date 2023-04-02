a3589d88b5eddcc613f83d68dbfcb8edeaca1a03
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-02 17:08:35 +0300

    updated auth controller

--- controllers/auth.contollers.js
+++ controllers/auth.contollers.js
@@ -4,33 +4,33 @@ const jwt = require("jsonwebtoken");
 exports.register = async (req, res) => {
   const { email, password, role } = req.body;
 
-  const existingUser = await User.findOne({ email });
+  const existingStudent = await Student.findOne({ email });
 
-  if (existingUser)
+  if (existingStudent)
     return res.status(409).json({ message: "Email already exists" });
 
-  const user = new User();
-  user.email = email;
-  user.password = password;
+  const student = new Student();
+  student.email = email;
+  student.password = password;
 
   if (role) user.role = role;
-  await user.save();
+  await student.save();
   const { password: hashedPassword, ...newUser } = user.toJSON();
   res.status(201).json(newUser);
 };
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const student = await Student.findOne({ email });
 
-  if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+  if (!student) return res.status(404).json({ message: "Invalid Credentials" });
 
-  const isMatched = user.matchPassword(password);
+  const isMatched = student.matchPassword(password);
   if (!isMatched)
     return res.status(404).json({ message: "Invalid Credentials" });
 
   const token = jwt.sign(
-    { id: user._id, email: user.email },
+    { id: student._id, email: student.email },
     process.env.SECRET_KEY
   );
 
