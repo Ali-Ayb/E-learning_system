8f9ce977d4de39173ece75cdd1dcda937bd31726
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-02 21:09:42 +0300

    added file extension in file controller

--- controllers/file.controllers.js
+++ controllers/file.controllers.js
@@ -4,6 +4,7 @@ exports.addFile = async (req, res) => {
   try {
     const base64File = req.body.file;
     const file_name = req.body.file_name;
+    const file_ext = req.body.ext;
 
     const fileBuffer = Buffer.from(base64File, "base64");
 
@@ -13,7 +14,7 @@ exports.addFile = async (req, res) => {
     const filePath = path.join(__dirname, "..", "uploads", uniqueFileName);
     await fs.promises.writeFile(filePath, fileBuffer);
 
-    const file = new File({ name: file_name, path: filePath });
+    const file = new File({ name: file_name + "." + file_ext, path: filePath });
     await file.save();
 
     res.status(200).json({ message: "File uploaded successfully" });
