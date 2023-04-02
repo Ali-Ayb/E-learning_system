e428f3ff67450480ec575ea6636ab61a94addc9c
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-02 17:03:32 +0300

    updated create course in course controller where now it check if course exits before adding it

--- controllers/course.controllers.js
+++ controllers/course.controllers.js
@@ -3,12 +3,12 @@ const Course = require("../models/courseModel.js");
 exports.createCourse = async (req, res) => {
   const { name, description } = req.body;
 
-  const course = await Course.create({ name, description });
-
   const existingCourse = await Course.findOne({ name });
 
   if (existingCourse)
     return res.status(409).json({ message: "Course already exists" });
+
+  const course = await Course.create({ name, description });
 
   res.json(course);
 };
